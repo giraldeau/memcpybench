0ddb0f84f06ba718f83ff2b005d4f1c1a585fc29
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-01-11 22:29:17 -0500

    Add results
    Conclusion: SIMD is as fast as ERMS.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <chrono>
 #include <typeinfo>
+#include <functional>
+#include <cassert>
 #include <x86intrin.h>
 
 using namespace std;
@@ -57,9 +60,9 @@ public:
 };
 
 template<typename T>
-class MemcpySIMD : public Memcpy<T> {
+class MemcpySIMDsi : public Memcpy<T> {
 public:
-    MemcpySIMD() : Memcpy<T>("simd") {}
+    MemcpySIMDsi() : Memcpy<T>("simd_si") {}
     void copy(vector<T> &dst, vector<T> &src) {
         int chunk = (16 / sizeof(T));
         int end = src.size() / chunk;
@@ -70,37 +73,94 @@ public:
     }
 };
 
+template<typename T>
+class MemcpySIMDps : public Memcpy<T> {
+public:
+    MemcpySIMDps() : Memcpy<T>("simd_ps") {}
+    void copy(vector<T> &dst, vector<T> &src) {
+        float *src_data = (float *)(src.data());
+        float *dst_data = (float *)(dst.data());
+        uint n = src.size() / 4;
+        for (uint i = 0; i < n; i += 4) {
+            __m128 A = _mm_loadu_ps(src_data + i);
+            _mm_storeu_ps(dst_data + i, A);
+        }
+    }
+};
+
+float benchmark_lambda(int &repeat_min,
+                      chrono::microseconds &delay_min,
+                      std::function<void ()> func) {
+    chrono::microseconds elapsed = chrono::microseconds::zero();
+    int repeat = 0;
+    while (repeat < repeat_min && elapsed < delay_min) {
+        auto t1 = chrono::steady_clock::now();
+        func();
+        auto t2 = chrono::steady_clock::now();
+        elapsed += chrono::duration_cast<chrono::microseconds>(t2 - t1);
+        repeat++;
+    }
+    return elapsed.count() / repeat;
+}
+
+void print_results(map<string, map<int, float>> &results) {
+    // header
+    for (const auto &item: results) {
+        cout << item.first << ",";
+    }
+    cout << endl;
+
+    // data
+    if (results.size() > 0) {
+        map<int, float> keys = results.begin()->second;
+        for (const auto &key: keys) {
+            int power = key.first;
+            cout << power << ",";
+            for (auto &item: results) {
+                //item.first; // name
+                //item.second; // map
+                cout << item.second[power] << ",";
+            }
+            cout << endl;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     (void) argc; (void) argv;
 
-    int n = 1024 * 16;
-    vector<char> src(n), dst(n);
-
-    vector<Memcpy<char>* > flavor = {
+    vector<Memcpy<char>* > flavors = {
         new MemcpyScalar<char>(),
         new MemcpyStd<char>(),
         new MemcpyErms<char>(),
-        new MemcpySIMD<char>(),
+        new MemcpySIMDsi<char>(),
+        new MemcpySIMDps<char>(),
     };
 
-    for (auto fl: flavor) {
-        // reset
-        fill(src.begin(), src.end(), 42);
-        fill(dst.begin(), dst.end(), 0);
+    int repeat_min = 10E5;
+    auto delay_min = chrono::microseconds(100000); // 100ms min
+    int power_max = 20;
+    map<string, map<int, float>> results; // (power, avg_us)
 
-        chrono::microseconds elapsed = chrono::microseconds::zero();
-        auto min_delay = chrono::microseconds(1000000);
-        int repeat = 0;
-        while (repeat < 1000 && elapsed < min_delay) {
-            auto t1 = chrono::steady_clock::now();
-            fl->copy(dst, src);
-            auto t2 = chrono::steady_clock::now();
-            elapsed += chrono::duration_cast<chrono::microseconds>(t2 - t1);
-            repeat++;
+    for (int power = 5; power <= power_max; power++) {
+        int n = (1 << power);
+        vector<char> src(n), dst(n);
+        for (auto flavor: flavors) {
+            cout << flavor->m_name << " " << power << endl;
+            fill(src.begin(), src.end(), 42);
+            fill(dst.begin(), dst.end(), 0);
+            float elapsed = benchmark_lambda(repeat_min, delay_min,
+                [&]() {
+                    flavor->copy(dst, src);
+                }
+            );
+            results[flavor->m_name][power] = elapsed;
+            assert(dst[n - 1] == 42);
         }
-        std::cout << fl->m_name << ": " << elapsed.count() << std::endl;
     }
+
+    print_results(results);
 
     return 0;
 }
